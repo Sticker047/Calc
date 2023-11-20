5593c8ef1e520b5f643e4932fb66aa2a8b534f83
Sticker047 <49759033+Sticker047@users.noreply.github.com>
2023-11-20 21:06:49 +0500

    fix range of numbers + count of args

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     private static final String[] ARAB = "0123456789".split("");
@@ -17,9 +18,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
 //        System.out.println(calc("1 + 4"));
 //        System.out.println(calc("VI / III"));
-        System.out.println(calc("VII * II"));
+//        System.out.println(calc("VII * II"));
 //        System.out.println(calc("I + 1"));
 //        System.out.println(calc("1 + 2 + 3"));
+
+        Scanner scanner = new Scanner(System.in);
+        calc(scanner.next());
     }
 
 
@@ -27,28 +31,59 @@ public class Main {
         checkInput(input);
         input = input.replace(" ", "");
         if (type == Type.ARAB) {
-            String[] args = input.split("");
-            switch (args[1]) {
-                case "+": {
-                    return String.valueOf(Integer.parseInt(args[0]) + Integer.parseInt(args[2]));
+            String[] args = input.split("\\+");
+            if (args.length == 2) {
+                for (String el : args) {
+                    if (Integer.parseInt(el) > 10 || Integer.parseInt(el) <= 10) {
+                        throw new Exception();
+                    }
                 }
-                case "-": {
-                    return String.valueOf(Integer.parseInt(args[0]) - Integer.parseInt(args[2]));
-                }
-                case "*": {
-                    return String.valueOf(Integer.parseInt(args[0]) * Integer.parseInt(args[2]));
-                }
-                case "/": {
-                    return String.valueOf(Integer.parseInt(args[0]) / Integer.parseInt(args[2]));
-                }
-                default: {
-                    System.out.println(args[1]);
+                return String.valueOf(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+            } else if (args.length == 1) {
+                args = args[0].split("-");
+                if (args.length == 2) {
+                    for (String el : args) {
+                        if (Integer.parseInt(el) > 10 || Integer.parseInt(el) <= 10) {
+                            throw new Exception();
+                        }
+                    }
+                    return String.valueOf(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
+                } else if (args.length == 1) {
+                    args = args[0].split("\\*");
+                    if (args.length == 2) {
+                        for (String el : args) {
+                            if (Integer.parseInt(el) > 10 || Integer.parseInt(el) <= 10) {
+                                throw new Exception();
+                            }
+                        }
+                        return String.valueOf(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
+                    } else if (args.length == 1) {
+                        args = args[0].split("/");
+                        if (args.length == 2) {
+                            for (String el : args) {
+                                if (Integer.parseInt(el) > 10 || Integer.parseInt(el) <= 10) {
+                                    throw new Exception();
+                                }
+                            }
+                            return String.valueOf(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
+                        } else {
+                            throw new Exception();
+                        }
+                    } else {
+                        throw new Exception();
+                    }
+                } else {
                     throw new Exception();
                 }
+            } else {
+                throw new Exception();
             }
         } else if (type == Type.ROME) {
             if (input.contains("+")) {
                 String[] split = input.split("\\+");
+                if (split.length != 2) {
+                    throw new Exception();
+                }
                 if (str.contains(split[0]) && str.contains(split[1])) {
                     int result = (str.indexOf(split[0]) + 1) + (str.indexOf(split[1]) + 1);
                     if (result <= 0) {
@@ -64,6 +99,9 @@ public class Main {
                 }
             } else if (input.contains("-")) {
                 String[] split = input.split("-");
+                if (split.length != 2) {
+                    throw new Exception();
+                }
                 if (str.contains(split[0]) && str.contains(split[1])) {
                     return convertFromRomeToArab((str.indexOf(split[0]) + 1) - (str.indexOf(split[1]) + 1));
                 } else {
@@ -71,6 +109,9 @@ public class Main {
                 }
             } else if (input.contains("*")) {
                 String[] split = input.split("\\*");
+                if (split.length != 2) {
+                    throw new Exception();
+                }
                 if (str.contains(split[0]) && str.contains(split[1])) {
                     return convertFromRomeToArab((str.indexOf(split[0]) + 1) * (str.indexOf(split[1]) + 1));
                 } else {
@@ -78,6 +119,9 @@ public class Main {
                 }
             } else if (input.contains("/")) {
                 String[] split = input.split("/");
+                if (split.length != 2) {
+                    throw new Exception();
+                }
                 if (str.contains(split[0]) && str.contains(split[1])) {
                     return convertFromRomeToArab((str.indexOf(split[0]) + 1) / (str.indexOf(split[1]) + 1));
                 } else {
@@ -115,12 +159,15 @@ public class Main {
         }
     }
 
-    private static String convertFromRomeToArab(int input) {
+    private static String convertFromRomeToArab(int input) throws Exception {
+        if (input <= 0) {
+            throw new Exception();
+        }
         String res = "";
         if (input / 10 > 0) {
-            res = res.concat(str2.get(input/10 - 1));
+            res = res.concat(str2.get(input / 10 - 1));
         }
-        res = res.concat(str.get(input%10 - 1));
+        res = res.concat(str.get(input % 10 - 1));
         return res;
     }
 
